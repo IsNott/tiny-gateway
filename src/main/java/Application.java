be0874382c9f4b8a24940519df0506b1b9d0728d
Zzzwlong <1448832395@qq.com>
2023-08-09 21:44:23 +0800

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Nott
+ * @Date 2023/8/9
+ */
+
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+//@EnableFeignClients
+@ComponentScan(basePackages = "com.nott")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
