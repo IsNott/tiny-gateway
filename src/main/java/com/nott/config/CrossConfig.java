f5cfe9c00e1df9fac551f1dee0610e579b5f3cad
Zzzwlong <1448832395@qq.com>
2023-09-03 22:04:38 +0800

    add: CrossConfig

---
+++ src/main/java/com/nott/config/CrossConfig.java
@@ -0,0 +1,32 @@
+package com.nott.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Nott
+ * @Date 2023/8/11
+ */
+
+
+@Configuration
+public class CrossConfig {
+    @Bean
+    public WebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
