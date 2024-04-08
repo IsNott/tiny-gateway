62a7421ef62e7a99805d19bf58ce3db975b37fe7
Nott <nott@163.com>
2024-04-08 12:01:13 +0800

    update:nacos config\discovery version add: logback、nacos-example.xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
